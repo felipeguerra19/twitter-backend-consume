4a2bd7984bda94f734ae8e3c788dae3a8b845763
Felipe Guerra de Oliveira <felipeguerraoliveira@protonmail.com>
2019-04-16 09:55:33 -0300

    Testa o valor do JSON não da String

--- test/TwitterLoadTest.js
+++ test/TwitterLoadTest.js
@@ -11,9 +11,10 @@ describe('Carregar e ler os tweets do arquivo', function() {
         it('Tem que carregar o arquivo com sucesso', function(done) {
             try {
                 //lendo o arquivo no mesmo path
-                let binary_twitees = fs.readFileSync('/Users/felipeguerraoliveira/Projetos/Exams/Itau/Backend/twitter-backend-consume/test/twitter_search_api.json',
-                                                    'utf8');
-                expect(binary_twitees).not.to.equal(null);
+                let json_file = __dirname + '/twitter_search_api.json';
+                let binary_twitees = fs.readFileSync(json_file, 'utf8');
+                let json_twitees = JSON.parse(JSON.stringify(binary_twitees));
+                expect(json_twitees).not.to.equal(null);
                 done();
 
             }catch(e) {
